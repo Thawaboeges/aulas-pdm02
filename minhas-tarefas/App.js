7e54b908844967c97b8a78f24ee41050c4de3cbd
Thawaboeges <thawa.borges@gmail.com>
2026-03-11 23:04:39 -0300

    Atualização no App.js

--- minhas-tarefas/App.js
+++ minhas-tarefas/App.js
@@ -1,50 +1,24 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
-
-// Importações de arquivos locais
-import { titulo } from './util';
-import titulo_padrao from './util';
-import { 
-  rotulo_input_meta, 
-  rotulo_btn_cadastro_meta, 
-  rotulo_lista_metas 
-} from './mensagens';
+import {  rotulo_input_meta, rotulo_btn_cadastro_meta, rotulo_lista_metas } from './mensagens';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* Seção das Metas (Substituindo HTML por React Native) */}
-      <Text style={styles.header}>{rotulo_lista_metas}</Text>
-      
-      <TextInput 
-        style={styles.input} 
-        placeholder={rotulo_input_meta} 
-      />
-      
-      <Button 
-        title={rotulo_btn_cadastro_meta} 
-        onPress={() => alert('Meta Adicionada!')} 
-      />
-
-      <View style={styles.mainContainer} />
-
-
-      {/* Seção Original do seu App */}
-      <Text>{titulo}</Text>
-      <Text style={{ margin: 20 }}>{titulo_padrao}</Text>
-      <Text style={styles.text}>AAA</Text>
-      
-      <Button 
-        title="Clique aqui" 
-        onPress={() => alert('Botão clicado!')} 
-      />
-      
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View>
+        <TextInput style={styles.inputText} placeholder={rotulo_input_meta}/>
+      </View>
+      <View>
+        <Button title={rotulo_btn_cadastro_meta}/>
+      </View>
+      <View>
+        <Text>{rotulo_lista_metas}</Text>
+      </View>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -59,26 +33,9 @@ const styles = StyleSheet.create({
     padding: 30,
   },
 
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  input: {
-    width: '100%',
+  TextInput: {
     borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-    marginBottom: 10,
-    borderRadius: 5,
+    borderColor: '#cccccc',
   },
-  text: {
-    margin: 20,
-  },
-  separator: {
-    height: 1,
-    backgroundColor: '#eee',
-    width: '100%',
-    marginVertical: 20,
-  }
+
 });
